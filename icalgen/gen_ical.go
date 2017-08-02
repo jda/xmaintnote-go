41fbacf0233b2fefab1858cfc9daf2f47b654203
Jade Auer <jda@tapodi.net>
2017-08-01 23:32:46 -0700

    icalgen is kinda hacky working

---
+++ icalgen/gen_ical.go
@@ -0,0 +1,121 @@
+// Package icalgen provides just enough ical support to generate valid
+// ical files from the subset of ical used by xmaintnote-go
+package icalgen
+
+import (
+	"fmt"
+	"strings"
+
+	icu "github.com/jda/xmaintnote-go/icalutils"
+	"github.com/luxifer/ical"
+)
+
+// TODO: swap the out, append, out, sprintf for func that encapsulates
+// all that junk and handles wrapping of lines that are too long
+// TODO use reader/writer interface?
+// TODO clean up, properly implement RFC, then upstream
+
+// Export exports a ical.Calendar in ical format as a byte array
+func Export(c *ical.Calendar) (out []byte, err error) {
+	out = append(out, "BEGIN:VCALENDAR\r\n"...)
+
+	version := icu.GetPropVal(c.Properties, "VERSION")
+	if version != "" {
+		out = append(out, fmt.Sprintf("VERSION:%s\r\n", version)...)
+	}
+
+	prodid := icu.GetPropVal(c.Properties, "PRODID")
+	if prodid != "" {
+		out = append(out, fmt.Sprintf("PRODID:%s\r\n", prodid)...)
+	}
+
+	for ei := range c.Events {
+		event := c.Events[ei]
+
+		out = append(out, "BEGIN:VEVENT\r\n"...)
+
+		if event.Summary != "" {
+			out = append(out, fmt.Sprintf("SUMMARY:%s\r\n", event.Summary)...)
+		}
+
+		if event.StartDate.IsZero() == false {
+			d := event.StartDate.Format("20060102T150405")
+			out = append(out, fmt.Sprintf("DTSTART;VALUE=DATE-TIME:%s\r\n", d)...)
+		}
+
+		if event.EndDate.IsZero() == false {
+			d := event.EndDate.Format("20060102T150405")
+			out = append(out, fmt.Sprintf("DTEND;VALUE=DATE-TIME:%s\r\n", d)...)
+		}
+
+		if event.Timestamp.IsZero() == false {
+			d := event.Timestamp.UTC().Format("20060102T150405Z")
+			out = append(out, fmt.Sprintf("DTSTAMP;VALUE=DATE-TIME:%s\r\n", d)...)
+		}
+
+		if event.UID != "" {
+			out = append(out, fmt.Sprintf("UID:%s\r\n", event.UID)...)
+		}
+
+		sequence := icu.GetPropVal(event.Properties, "SEQUENCE")
+		if sequence != "" {
+			out = append(out, fmt.Sprintf("SEQUENCE:%s\r\n", sequence)...)
+		}
+
+		organizer, err := getFormatOrganizer(event)
+		if err != nil {
+			return out, err
+		}
+		if organizer != "" {
+			out = append(out, fmt.Sprintf("%s\r\n", organizer)...)
+		}
+
+		xProperties := genXProperties(event)
+		out = append(out, xProperties...)
+
+		out = append(out, "END:VEVENT\r\n"...)
+	}
+
+	out = append(out, "END:VCALENDAR\r\n"...)
+	return out, nil
+}
+
+func genXProperties(e *ical.Event) (seq []byte) {
+	for pi := range e.Properties {
+		prop := e.Properties[pi]
+		if strings.HasPrefix(prop.Name, "X-") == true {
+			line := prop.Name
+			if len(prop.Params) == 0 {
+				line += fmt.Sprintf(":%s", prop.Value)
+			} else {
+				for pmName := range prop.Params {
+					param := prop.Params[pmName]
+					line += fmt.Sprintf(";%s=\"%s\"", pmName, param.Values[0])
+				}
+				line += fmt.Sprintf(":%s", prop.Value)
+			}
+			seq = append(seq, fmt.Sprintf("%s\r\n", line)...)
+		}
+	}
+	return seq
+}
+
+func getFormatOrganizer(e *ical.Event) (fo string, err error) {
+	for pi := range e.Properties {
+		if e.Properties[pi].Name != "ORGANIZER" {
+			continue
+		}
+		fo = "ORGANIZER"
+
+		// TODO read spec and do the right thing here
+		pr := e.Properties[pi]
+		if v, ok := pr.Params["CN"]; ok {
+			cn := v.Values[0]
+			fo += fmt.Sprintf(`;CN="%s"`, cn)
+		}
+		email := pr.Value
+		fo += fmt.Sprintf(":%s", email)
+		return fo, nil
+	}
+	return fo, nil
+}
---
+++ icalgen/gen_ical_test.go
@@ -0,0 +1,85 @@
+package icalgen
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/google/go-cmp/cmp"
+	"github.com/luxifer/ical"
+)
+
+var testCaseNoXMaint = `BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//Maint Note//https://github.com/maint-notification//
+BEGIN:VEVENT
+SUMMARY:Maint Note Example
+DTSTART;VALUE=DATE-TIME:20160612T200000
+DTEND;VALUE=DATE-TIME:20160612T210000
+DTSTAMP;VALUE=DATE-TIME:20160612T192352Z
+UID:42
+SEQUENCE:1
+ORGANIZER;CN="Example NOC":mailto:noone@example.com
+END:VEVENT
+END:VCALENDAR`
+
+var testCaseWithXMaint = `BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//Maint Note//https://github.com/maint-notification//
+BEGIN:VEVENT
+SUMMARY:Maint Note Example
+DTSTART;VALUE=DATE-TIME:20160612T200000
+DTEND;VALUE=DATE-TIME:20160612T210000
+DTSTAMP;VALUE=DATE-TIME:20160612T192352Z
+UID:42
+SEQUENCE:1
+X-MAINTNOTE-PROVIDER:example.com
+X-MAINTNOTE-ACCOUNT:137.035999173
+X-MAINTNOTE-MAINTENANCE-ID:WorkOrder-31415
+X-MAINTNOTE-OBJECT-ID;ALTREP="https://example.org/maintenance?id=acme-widg
+ ets-as-a-service":acme-widgets-as-a-service
+X-MAINTNOTE-IMPACT:NO-IMPACT
+X-MAINTNOTE-STATUS:TENTATIVE
+ORGANIZER;CN="Example NOC":mailto:noone@example.com
+END:VEVENT
+END:VCALENDAR
+`
+
+// test generating and then parsing a ical without xmaintnote extensions
+func TestGenerateICal(t *testing.T) {
+	doTestCase(t, testCaseNoXMaint)
+}
+
+// test generating and then parsing a ical with xmaintnote extensions
+func TestGenerateXMaintNoteICal(t *testing.T) {
+	doTestCase(t, testCaseWithXMaint)
+}
+
+func doTestCase(t *testing.T, testCase string) {
+	// test setup
+	lfTestCase := strings.Replace(testCase, "\n", "\r\n", -1)
+	inIcalReader := strings.NewReader(lfTestCase)
+	inIcal, err := ical.Parse(inIcalReader)
+	if err != nil {
+		t.Fatalf("ical parse error on test setup: %s", err)
+	}
+
+	// test output
+	outIcal, err := Export(inIcal)
+	if err != nil {
+		t.Fatalf("ical export error in test: %s", err)
+	}
+
+	// parse results from output and compare to origional parse
+	generatedIcalReader := bytes.NewReader(outIcal)
+	generatedIcal, err := ical.Parse(generatedIcalReader)
+	if err != nil {
+		t.Fatalf("could not parse generated ical: %s", err)
+	}
+	spew.Dump(generatedIcal)
+
+	if diff := cmp.Diff(inIcal, generatedIcal); diff != "" {
+		t.Fatalf("generated ical does not match origional ical: (-got +want)\n%s", diff)
+	}
+}
