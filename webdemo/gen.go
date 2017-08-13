4b5de90b6d41b43f16f0ceb5a1b3401caa3c3e5f
Jade Auer <jda@tapodi.net>
2017-08-12 22:31:57 -0700

    added ical generation

--- webdemo/gen.go
+++ webdemo/gen.go
@@ -1,45 +1,91 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
+	"bytes"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/golang/glog"
+	"github.com/google/uuid"
+	xmaintnote "github.com/jda/xmaintnote-go"
 )
 
-var genHTML = `<html><head>
-<title>Make a Maint notice</title>
-</head><body>
-<h1>Make a xmaintnote ical</h1><p>
-<form method="POST">
-Summary:
-Provider:
-Account:
-Maintenance ID:
-Sequence:
-Organizer Email
-Impact
-Status
-Start
-End
-Maintenance Objects (at least one)
-Name
-Data
-<input type="submit" value="generate">
-</form>
-</p></body></html>`
-var genTmpl = template.Must(template.New("gen_in").Parse(genHTML))
+const inputTimePattern string = "2006-01-02T15:04"
 
 // handle generation and download
 func genHandler(w http.ResponseWriter, r *http.Request) {
 	glog.Infof("request: %s %s %s", r.RemoteAddr, r.Method, r.RequestURI)
 	switch r.Method {
 	case "GET":
-		fmt.Fprint(w, genHTML)
+		renderTemplate(w, "page_gen.tmpl", nil)
 	case "POST":
-		uploadHandler(w, r)
+		genFormHandler(w, r)
 	default:
 		nothingHandler(w, r)
 	}
+}
+
+func genFormHandler(w http.ResponseWriter, r *http.Request) {
+	tmplVars := make(map[string]interface{})
+
+	mn := xmaintnote.NewMaintNote()
+	maintEvent := xmaintnote.MaintEvent{
+		Summary:        r.FormValue("summary"),
+		Provider:       r.FormValue("provider"),
+		Account:        r.FormValue("account"),
+		MaintenanceID:  r.FormValue("maintid"),
+		Impact:         r.FormValue("impact"),
+		Status:         r.FormValue("status"),
+		OrganizerEmail: r.FormValue("organizer"),
+		Created:        time.Now().UTC(),
+		UID:            uuid.New().String(),
+	}
+
+	startTime, err := time.Parse(inputTimePattern, r.FormValue("start"))
+	if err != nil {
+		glog.Warning("failed to parse startTime")
+	}
+	maintEvent.Start = startTime
+
+	endTime, err := time.Parse(inputTimePattern, r.FormValue("end"))
+	if err != nil {
+		glog.Warning("failed to parse endTime")
+	}
+	maintEvent.End = endTime
+
+	rawObjs := r.FormValue("maintobject")
+	objs := strings.Split(rawObjs, "\r\n")
+	for _, obj := range objs {
+		parts := strings.SplitN(obj, ",", 2)
+		if len(parts) != 2 {
+			glog.Warning("ignoring maintobject, got wrong num of parts")
+			continue
+		}
+		maintObj := xmaintnote.MaintObject{
+			Name: parts[0],
+			Data: parts[1],
+		}
+		maintEvent.Objects = append(maintEvent.Objects, maintObj)
+	}
+
+	_, err = maintEvent.IsValid()
+	if err != nil {
+		tmplVars["Error"] = err.Error()
+		glog.Errorf("maintEvent is not valid: `%s`", err)
+		renderTemplate(w, "page_gen.tmpl", tmplVars)
+		return
+	}
+
+	mn.Events = []xmaintnote.MaintEvent{maintEvent}
+
+	calBytes := mn.Export()
+	cal := bytes.NewBuffer(calBytes)
+
+	fname := maintEvent.MaintenanceID + ".ics"
+
+	w.Header().Set("Content-Disposition", "attachment; filename="+fname)
+	w.Header().Set("Content-Type", "text/calendar")
+
+	cal.WriteTo(w)
 }
